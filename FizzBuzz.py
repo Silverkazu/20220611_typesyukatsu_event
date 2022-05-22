896a05fd4c67a8e597bab894fcc891135b64d695
takashima <takashimak@g.ecc.u-tokyo.ac.jp>
2022-05-22 23:22:33 +0900

    Modify small

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -6,11 +6,19 @@
 """
 
 for num in range(1, 101):
+    string = ''
+
+    # 15の倍数のときは数字の代わりにFizzBuzzと出力
     if num % 3 == 0 and num % 5 == 0:
-        print("FizzBuzz")
+        string = "FizzBuzz"
+    # 3の倍数のときは数字の代わりにFizzと出力
     elif num % 3 == 0:
-        print("Fizz")
+        string = "Fizz"
+    # 5の倍数のときは数字の代わりにBuzzと出力
     elif num % 5 == 0:
-        print("Buzz")
+        string = "Buzz"
+    # 3, 5どちらの倍数でもない時は、その数字を出力
     else:
-        print(num)
+        string = str(num)
+
+    print(string)
