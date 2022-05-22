15175ea92376dcd9da54d70e683b6146aecf2805
takashima <takashimak@g.ecc.u-tokyo.ac.jp>
2022-05-22 22:48:32 +0900

    Modify small

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -4,9 +4,9 @@
 5の倍数のときは数字の代わりにBuzzと出力
 15の倍数のときは数字の代わりにFizzBuzzと出力
 """
-ini = 1
-end = 100
-for num in range(ini, end+1):
+begin_num = 1
+end_num = 100
+for num in range(begin_num, end_num+1):
     if num % 3 == 0 and num % 5 == 0:
         print("FizzBuzz")
     elif num % 3 == 0:
